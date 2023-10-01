09d81acffaac7cc29aa43ba7dd21e6d7b2765e9c
suzan khanal <khanalsuzan66@gmail.com>
2023-10-01 13:20:32 +0545

    Initial Commit

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace E_commerce;
+
+public class ApplicationDBContext : DbContext
+{
+    public ApplicationDBContext(DbContextOptions<ApplicationDBContext>options):base(options)//Passing Options to Database
+    {
+        
+    }
+    public DbSet<Product>Products{get; set;}
+
+}
---
+++ Models/Product.cs
@@ -0,0 +1,7 @@
+﻿namespace E_commerce;
+
+public class Product
+{
+    public int ID { get; set;}
+    public string Name { get; set;} 
+}
